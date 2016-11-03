a0b9816fb688a54b3acc8791fdf107c642cba369
jerrychadwick <jerrychadwickjr@gmail.com>
2016-11-03 16:02:48 -0600

    finished assignment (v 0.9) ;-)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@
  */
 
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientHandlerException;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
@@ -47,7 +48,7 @@ public class Main {
     }
 
 
-    private static FamilySearchPlatform readGedcomx(String url, String sessionId) {
+    private static FamilySearchPlatform readGedcomxTree(String url, String sessionId) {
 //    private static Gedcomx readGedcomx(String url, String sessionId, String mediaType) {
         WebResource.Builder builder = client.resource(url).getRequestBuilder().accept(FS_PLATFORM_V1_XML_MEDIA_TYPE);
         if (sessionId != null) {
@@ -55,17 +56,39 @@ public class Main {
             builder = builder.header("From", "Jerry Chadwick");
         }
         ClientResponse response = builder.get(ClientResponse.class);
-        if (response.getStatus() == 200) {
+        if (response.getStatus() == 200 && response.getType().toString().equals(FS_PLATFORM_V1_XML_MEDIA_TYPE)) {
             return response.getEntity(FamilySearchPlatform.class);
         }
         System.out.println("Error: got response code " + response.getStatus() + " for URL " + url);
         return null;
     }
 
+    private static Gedcomx readGedcomxRecords(String url, String sessionId) {
+        WebResource.Builder builder = client.resource(url).getRequestBuilder().accept(GEDCOMX_XML_MEDIA_TYPE);
+        if (sessionId != null) {
+            builder = builder.header("Authorization", "Bearer " + sessionId);
+            builder = builder.header("From", "Jerry Chadwick");
+        }
+        try {
+            ClientResponse response = builder.get(ClientResponse.class);
+            if (response.getStatus() == 200 && response.getType().toString().equals(GEDCOMX_XML_MEDIA_TYPE)) {
+                return response.getEntity(Gedcomx.class);
+            }
+            else
+                System.out.println("Error: got response code " + response.getStatus() + " for URL " + url);
+        }
+        catch (ClientHandlerException e) {
+            //do something clever with the exception
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
+
     private  static void getPersonAndParents(String treePersonURL, int iLevel, String sessionId) {
         iAncestorCount++;
         // Read person
-        FamilySearchPlatform treePerson = (FamilySearchPlatform) readGedcomx(treePersonURL, sessionId);
+        FamilySearchPlatform treePerson = (FamilySearchPlatform) readGedcomxTree(treePersonURL, sessionId);
         String parentLabel;
         switch (iLevel) {
             case 1: parentLabel = "Me"; break;
@@ -76,45 +99,72 @@ public class Main {
             case 6: parentLabel = "3rd Great-Grandparent"; break;
             default: parentLabel = iLevel + "th Great-Grandparent";
         }
-        printPersonSummary(treePerson, "Tree person  -- Relation = " + parentLabel);
+        String sTreePersonName = printPersonSummary(treePerson, "Family Tree person details -- Relation = " + parentLabel);
+        System.out.println();
+        System.out.println("Sources for " + sTreePersonName + ":");
+        System.out.println();
 
         List<Person> personsArray = treePerson.getPersons();
-
         if (personsArray != null) {
             List<SourceReference> sourcesRefList = personsArray.get(0).getSources();
             if (sourcesRefList != null) {
                 for (SourceReference sourceRef : sourcesRefList) {
                     URI sourceURI = sourceRef.getDescriptionRef();
                     System.out.println(sourceURI.toString());
-                    FamilySearchPlatform recordPersona = readGedcomx(sourceURI.toString(), sessionId);
+                    Gedcomx recordPersona = readGedcomxRecords(sourceURI.toString(), sessionId);
                     List<SourceDescription> sourceDescList = recordPersona.getSourceDescriptions();
                     URI sourceDescURI = sourceDescList.get(0).getAbout();
                     if (sourceDescURI != null) {
-                        FamilySearchPlatform recordData = readGedcomx(sourceDescURI.toString(), sessionId);
+                        Gedcomx recordData = readGedcomxRecords(sourceDescURI.toString(), sessionId);
                         if (recordData != null) {
                             if (recordData.getPersons() != null) {
                                 for (Person persona : recordData.getPersons()) {
                                     if (persona != null) {
-                                        System.out.println("Persona name: " + persona.getName().toString());
-                                        URI factType;
-                                        Date factDate;
-                                        PlaceReference factPlace;
-                                        if (persona.getFacts() != null) {
-                                            for (Fact fact : persona.getFacts()) {
-                                                if (fact != null) {
-                                                    System.out.println("Fact info:");
+                                        if (persona.getName() != null) {
+                                            String sFactLabel = "";
+                                            String sFactDate = "";
+                                            String sFactPlace = "";
+                                            String sPersonaName = persona.getName().getNameForms().get(0).getFullText();
+                                            System.out.println("========== Persona name: " + sPersonaName + "==========");
+                                            String factType;
+                                            Date factDate;
+                                            PlaceReference factPlace;
+                                            if (persona.getFacts() != null) {
+                                                for (Fact fact : persona.getFacts()) {
+                                                    if (fact != null) {
+                                                        factDate = fact.getDate();
+                                                        if (factDate != null)
+                                                            sFactDate =  factDate.getOriginal();
 
-                                                    factType = fact.getType();
-                                                    if (factType != null)
-                                                        System.out.println("Persona Fact Type:   " + factType);
+                                                        factPlace = fact.getPlace();
+                                                        if (factPlace != null)
+                                                            sFactPlace = factPlace.getOriginal();
 
-                                                    factDate = fact.getDate();
-                                                    if (factDate != null)
-                                                        System.out.println("Persona Fact Date:   " + factDate.getOriginal());
-
-                                                    factPlace = fact.getPlace();
-                                                    if (factPlace != null)
-                                                        System.out.println("Persona Fact Place:   " + factPlace.getOriginal());
+                                                        // System.out.println("Fact info:");
+                                                        factType = fact.getType().toString().replaceAll(".*/", "");
+                                                        if (factType != null) {
+                                                            if (factType.equals("Census")) {
+                                                                sFactLabel = sPersonaName + " was found in the " + sFactDate + " census at " + sFactPlace;
+                                                            }
+                                                            else if (factType.equals("Birth")) {
+                                                                sFactLabel = sPersonaName + " was born in " + sFactDate + " at " + sFactPlace;
+                                                            }
+                                                            else if (factType.equals("Death")) {
+                                                                sFactLabel = sPersonaName + " died in " + sFactDate + " at " + sFactPlace;
+                                                            }
+                                                            else if (factType.equals("MaritalStatus")) {
+                                                                sFactLabel = sPersonaName + " was married in " + sFactDate + " at " + sFactPlace;
+                                                            }
+                                                            else if (factType.equals("Residence")) {
+                                                                sFactLabel = sPersonaName + " in " + sFactDate + " resided at " + sFactPlace;
+                                                            }
+                                                            else {
+                                                                sFactLabel = "Fact type: " + factType + "   Date: " + sFactDate + "   Place: " + sFactPlace;
+                                                            }
+                                                        }
+                                                        System.out.println(sFactLabel);
+                                                        System.out.println();
+                                                    }
                                                 }
                                             }
                                         }
@@ -127,6 +177,8 @@ public class Main {
                 }
             }
         }
+        System.out.println("End of " + sTreePersonName + " data =============");
+        System.out.println();
 
         if (treePerson.getRelationships() != null) {
             for (Relationship relationship : treePerson.getRelationships()) {
@@ -136,7 +188,7 @@ public class Main {
                     // compare the ID of the treePerson with Person2
                     if (relationship.getPerson2().getResourceId().equals(personID)) {
                         // treePerson is the child; i.e. this is a parent to treePerson
-                        FamilySearchPlatform parentPerson = (FamilySearchPlatform) readGedcomx(relationship.getPerson1().getResource().toString(), sessionId);
+                        FamilySearchPlatform parentPerson = (FamilySearchPlatform) readGedcomxTree(relationship.getPerson1().getResource().toString(), sessionId);
                         if (iLevel < NUMGENS) {
                             getPersonAndParents(FS_ARK_URL_BASE + relationship.getPerson1().getResourceId().toString() + "?sourceDescriptions", iLevel+1, sessionId);
                         }
@@ -144,11 +196,15 @@ public class Main {
                 }
             }
         }
+
     }
 
-    private static void printPersonSummary(FamilySearchPlatform gedcomx, String title) {
+    private static String printPersonSummary(FamilySearchPlatform gedcomx, String title) {
+        String retVal = "";
         System.out.println();
-        System.out.println(title + " ===============================");
+        System.out.println("=============================================================================================================");
+        System.out.println(title + ":");
+        System.out.println();
 
         if (gedcomx != null) {
             if (gedcomx.getPersons() != null) {
@@ -159,7 +215,8 @@ public class Main {
                             if (name.getNameForms() != null) {
                                 for (NameForm nameForm : name.getNameForms()) {
                                     if (nameForm.getFullText() != null) {
-                                        System.out.println("  Name: " + nameForm.getFullText());
+                                        retVal = nameForm.getFullText();
+                                        System.out.println("  Name: " + retVal);
                                     }
                                     if (nameForm.getParts() != null) {
                                         for (NamePart namePart : nameForm.getParts()) {
@@ -201,10 +258,13 @@ public class Main {
                             }
                         }
                     }
-                    System.out.println();
+
                 }
             }
         }
+
+        System.out.println();
+        return retVal;
     }
 
 
